8770ed7a21afb17d9ebbacc490a18621254bb6bd
fukumotom <a.word.of.kindness073@gmail.com>
2016-04-08 08:25:50 +0900

    param map creating

--- WorkManager/src/main/java/jp/co/ojt/dao/UserRegisterDao.java
+++ WorkManager/src/main/java/jp/co/ojt/dao/UserRegisterDao.java
@@ -2,15 +2,10 @@ package jp.co.ojt.dao;
 
 import java.util.ArrayList;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import jp.co.ojt.dao.dto.UsersDto;
 import jp.co.ojt.db.util.CommonDbUtil;
 
 public class UserRegisterDao {
-	
-	private static final Logger logger = LoggerFactory.getLogger(UserRegisterDao.class);
 
 	public void insert(UsersDto dto) {
 
@@ -18,10 +13,9 @@ public class UserRegisterDao {
 		String[] sqls = new String[2];
 		sqls[0] = CommonDbUtil.readSql("registUser.sql");
 		sqls[1] = CommonDbUtil.readSql("i_role.sql");
-		
-		// create sql parameter
-//		createParam(dto);
-		
+
+		// create sql parameter TODO
+
 		ArrayList<String> usersParamList = new ArrayList<>();
 		usersParamList.add(dto.getUserName());
 		usersParamList.add(dto.getPassword());
--- WorkManager/src/main/java/jp/co/ojt/dao/WorkDao.java
+++ WorkManager/src/main/java/jp/co/ojt/dao/WorkDao.java
@@ -1,7 +1,15 @@
 package jp.co.ojt.dao;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.sql.Time;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import jp.co.ojt.dao.dto.WorkDto;
 import jp.co.ojt.db.util.CommonDbUtil;
@@ -9,12 +17,16 @@ import jp.co.ojt.model.Work;
 
 public class WorkDao {
 
+	Logger logger = LoggerFactory.getLogger(Work.class);
+
 	public ArrayList<Work> findAllWork(Work work) {
 
 		// load sqlFile
-		String sql = CommonDbUtil.readSql("getWorkList");
+		String sql = CommonDbUtil.readSql("getWorkList.sql");
 
 		WorkDto dto = mappingModelToDto(work);
+
+		HashMap<Integer, Object> paramMap = createParamMap(sql, dto);
 
 		ArrayList<WorkDto> dtoList = CommonDbUtil.findAllWork(sql, dto);
 
@@ -25,6 +37,84 @@ public class WorkDao {
 		}
 
 		return workList;
+	}
+
+	private HashMap<Integer, Object> createParamMap(String sql, WorkDto dto) {
+
+		HashMap<String, Object> dtoMap = createDtoMap(dto);
+
+		HashMap<Integer, String> sqlParamMap = createSqlMap(sql);
+
+		HashMap<Integer, Object> paramMap = new HashMap<>();
+		for (String fieldName : dtoMap.keySet()) {
+
+			for (Integer index : sqlParamMap.keySet()) {
+
+			}
+
+		}
+
+		return paramMap;
+
+	}
+
+	private HashMap<Integer, String> createSqlMap(String sql) {
+		// sql文の動的パラメータとパラメータの順番のMapを作成
+		String regex = "${([a-zA-Z\\d]*)}))";
+		Pattern ptm = Pattern.compile(regex);
+
+		// SQL文からパラメータ代入箇所を取得
+		HashMap<Integer, String> sqlParamMap = new HashMap<>();
+		Matcher mat = ptm.matcher(sql);
+		if (mat.find()) {
+			int groupId = 1;
+
+			String sqlParam = mat.group(groupId);
+
+			sqlParamMap.put(groupId, sqlParam);
+
+		}
+
+		for (Integer key : sqlParamMap.keySet()) {
+			logger.info("Map内容[{}]:{}", key, sqlParamMap.get(key));
+		}
+		return sqlParamMap;
+	}
+
+	private HashMap<String, Object> createDtoMap(WorkDto dto) {
+		// Dtoのフィールド名と値のMapを作成
+		String regex = "get(([A-Z][a-zA-Z\\d]*))";
+		Pattern ptm = Pattern.compile(regex);
+
+		// Dtoのgetterからフィールド名を取得
+		Method[] methods = dto.getClass().getMethods();
+		HashMap<String, Object> dtoMap = new HashMap<>();
+
+		for (Method method : methods) {
+			// getterを抽出
+			Matcher mat = ptm.matcher(method.getName());
+			if (mat.find()) {
+				String getter = method.getName();
+				if (!getter.contains("Class")) {
+					String fieldName = mat.group(1);
+					fieldName = fieldName.substring(0, 1).toLowerCase() + fieldName.substring(1);
+
+					Object value = null;
+					try {
+						value = method.invoke(dto, null);
+					} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+						logger.info("リフレクション失敗", e);
+					}
+
+					dtoMap.put(fieldName, value);
+				}
+			}
+		}
+
+		for (String key : dtoMap.keySet()) {
+			logger.info("Map内容[{}]:{}", key, dtoMap.get(key));
+		}
+		return dtoMap;
 	}
 
 	private static Work mappingDtoToModel(WorkDto dto) {
