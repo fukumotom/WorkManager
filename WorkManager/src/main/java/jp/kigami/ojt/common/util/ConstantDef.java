0af7a8f9ecb942a29c9c165a9e3981e105f926f0
fukumotom <a.word.of.kindness073@gmail.com>
2016-05-31 19:54:11 +0900

    addtion constant definiyion issue#51

---
+++ WorkManager/src/main/java/jp/kigami/ojt/common/util/ConstantDef.java
@@ -0,0 +1,12 @@
+package jp.kigami.ojt.common.util;
+
+public class ConstantDef {
+
+	private ConstantDef() {
+	}
+
+	public static final String WOKING_STATE_WORKING = "作業中";
+
+	public static final String WOKING_STATE_NONWORKING = "未作業";
+
+}
