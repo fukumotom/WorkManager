44570fe7068f2a4936cc9e378e84effa97ba3a19
fukumotom <a.word.of.kindness073@gmail.com>
2016-06-06 10:37:25 +0900

    フィールドにコメント追加

--- WorkManager/src/main/java/jp/kigami/ojt/common/util/ConstantDef.java
+++ WorkManager/src/main/java/jp/kigami/ojt/common/util/ConstantDef.java
@@ -14,10 +14,18 @@ public class ConstantDef {
 	private ConstantDef() {
 	}
 
+	/**
+	 * 作業状況（作業中）
+	 */
 	public static final String WOKING_STATE_WORKING = "作業中";
 
+	/**
+	 * 作業状況（未作業）
+	 */
 	public static final String WOKING_STATE_NONWORKING = "未作業";
 
+	/**
+	 * エラーメッセージ（属性）
+	 */
 	public static final String ERROR_MSG = "errorMsg";
-
 }
