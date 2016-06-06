59ebe892ff14427ea833a3710d45d73eb2a93ba7
fukumotom <a.word.of.kindness073@gmail.com>
2016-06-06 10:12:27 +0900

    コメント追加

--- WorkManager/src/main/java/jp/kigami/ojt/common/util/ConstantDef.java
+++ WorkManager/src/main/java/jp/kigami/ojt/common/util/ConstantDef.java
@@ -1,12 +1,23 @@
 package jp.kigami.ojt.common.util;
 
+/**
+ * 定数定義クラス
+ * 
+ * @author btkigamim
+ *
+ */
 public class ConstantDef {
 
+	/**
+	 * プライベートコンストラクタ
+	 */
 	private ConstantDef() {
 	}
 
 	public static final String WOKING_STATE_WORKING = "作業中";
 
 	public static final String WOKING_STATE_NONWORKING = "未作業";
+
+	public static final String ERROR_MSG = "errorMsg";
 
 }
