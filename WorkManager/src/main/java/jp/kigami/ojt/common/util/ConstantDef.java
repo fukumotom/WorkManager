62278de6c23c331047c8522b8dd6a4cdd89c3210
fukumotom <a.word.of.kindness073@gmail.com>
2016-06-07 17:58:14 +0900

    フラグ判定用の定数を定義

--- WorkManager/src/main/java/jp/kigami/ojt/common/util/ConstantDef.java
+++ WorkManager/src/main/java/jp/kigami/ojt/common/util/ConstantDef.java
@@ -18,4 +18,15 @@ public class ConstantDef {
 	 * エラーメッセージ（属性）
 	 */
 	public static final String ERROR_MSG = "errorMsg";
+	
+	
+	/**
+	 * フラグ判定(on)
+	 */
+	public static final String FLG_ON = "true";
+
+	/**
+	 * フラグ判定(off)
+	 */
+	public static final String FLG_OFF = "false";
 }
