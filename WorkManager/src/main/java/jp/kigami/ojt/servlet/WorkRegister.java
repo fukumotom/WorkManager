4043d4da33c52e8fce73d09733e4bae49039f8b3
fukumotom <a.word.of.kindness073@gmail.com>
2016-06-07 17:51:38 +0900

    作業登録画面に入力チェックを実装

--- WorkManager/src/main/java/jp/kigami/ojt/servlet/WorkRegister.java
+++ WorkManager/src/main/java/jp/kigami/ojt/servlet/WorkRegister.java
@@ -12,21 +12,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import jp.kigami.ojt.common.exception.BindFormatException;
 import jp.kigami.ojt.common.exception.BusinessException;
 import jp.kigami.ojt.common.exception.SystemException;
 import jp.kigami.ojt.common.util.ConstantDef;
-import jp.kigami.ojt.common.util.ConvertToModelUtils;
 import jp.kigami.ojt.common.util.DateUtils;
 import jp.kigami.ojt.common.util.InputValidation;
 import jp.kigami.ojt.common.util.MsgCodeDef;
 import jp.kigami.ojt.common.util.PropertyUtils;
+import jp.kigami.ojt.common.util.ValidationResult;
+import jp.kigami.ojt.form.WorkFinishForm;
 import jp.kigami.ojt.form.WorkRegisterForm;
+import jp.kigami.ojt.form.WorkRegisterViewForm;
 import jp.kigami.ojt.logic.WorkLogic;
 import jp.kigami.ojt.model.Work;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 作業登録/完了処理
@@ -65,12 +66,12 @@ public class WorkRegister extends HttpServlet {
 
 		WorkLogic logic = new WorkLogic();
 
-		WorkRegisterForm form = new WorkRegisterForm();
+		// formに仕掛作業と作業状態フラグを設定
+		WorkRegisterViewForm form = new WorkRegisterViewForm();
 		List<Work> workList = logic.findWorking(inputWork);
 
 		if (workList.size() == 0) {
 			logger.info("仕掛処理なし");
-
 			form.setWork(null);
 			form.setWorkingFlg(false);
 		} else if (workList.size() == 1) {
@@ -87,7 +88,7 @@ public class WorkRegister extends HttpServlet {
 
 		// 作業開始時間(初期表示用)を設定
 		form.setNowTime(DateUtils.getNowTimeStr());
-		request.setAttribute("form", form);
+		request.setAttribute("viewForm", form);
 		RequestDispatcher dispatcher = request
 				.getRequestDispatcher("/WEB-INF/jsp/work/workRegistForm.jsp");
 
@@ -108,60 +109,22 @@ public class WorkRegister extends HttpServlet {
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		WorkRegisterForm form = setForm(request);
-
+		// ログインユーザ名を設定
 		Work inputWork = new Work();
 		String userName = request.getUserPrincipal().getName();
 		inputWork.setUserName(userName);
 
-		// idチェック
-		if (!InputValidation.idCheck(form.getId())) {
-			throw new SystemException("不正な入力値です。");
-		} else {
-			// id正常取得時に設定
-
-			inputWork.setId(Integer.valueOf(form.getId()));
-		}
-
+		// 作業終了処理
 		if (request.getParameter("finishBtn") != null) {
-			// 作業終了処理
-			try {
-				workFinish(inputWork);
-			} catch (BusinessException e) {
-				request.setAttribute(ConstantDef.ERROR_MSG, e.getMessage());
-			}
+			WorkFinishForm finshForm = setfinishForm(request);
 
-		} else if (request.getParameter("startBtn") != null) {
-
-			String workingState = request.getParameter("workingFlg");
-			boolean workingFlg = false;
-			try {
-				workingFlg = ConvertToModelUtils.convertBoolean(workingState);
-			} catch (BindFormatException e) {
-				logger.warn("入力値のバインドに失敗");
-				request.setAttribute(ConstantDef.ERROR_MSG, e.getErrMsg());
-			}
-
-			String startTime = request.getParameter("startTime");
-
-			if (!startTime.isEmpty() & startTime != null) {
-				inputWork.setStartTime(DateUtils.getFomatTime(startTime));
-			} else if (startTime.isEmpty()) {
-				// 未入力の場合、現在時間で開始
-				inputWork.setStartTime(DateUtils.getNowTime());
-			} else if (startTime != null) {
-				// 入力check
-				if (!InputValidation.isTime(startTime)) {
-					// ("不正な入力値です。");
-				}
-			}
-
-			String contents = (String) request.getParameter("contents");
-			String note = (String) request.getParameter("note");
-			logger.info("入力値：開始時間[{}] 作業内容[{}] 備考[{}]", startTime, contents,
-					note);
-
-			if (workingFlg) {
+			// 入力(id)チェック
+			String id = finshForm.getId();
+			if (!InputValidation.idCheck(id)) {
+				throw new SystemException("不正な入力");
+			} else {
+				// IDを設定
+				inputWork.setId(Integer.valueOf(id));
 
 				try {
 					workFinish(inputWork);
@@ -170,8 +133,34 @@ public class WorkRegister extends HttpServlet {
 				}
 			}
 
-			inputWork.setContents(contents);
-			inputWork.setNote(note);
+		} else if (request.getParameter("startBtn") != null) {
+
+			WorkRegisterForm registerForm = setRegisterForm(request);
+
+			// 入力チェック
+			ValidationResult result = inputCheckWhenStart(registerForm,
+					inputWork);
+			if (!result.isCheckResult()) {
+				request.setAttribute(ConstantDef.ERROR_MSG,
+						result.getErrorMsg());
+			}
+
+			logger.info("入力値：開始時間[{}] 作業内容[{}] 備考[{}]",
+					registerForm.getStartTime(), registerForm.getContents(),
+					registerForm.getNote());
+
+			if (ConstantDef.FLG_ON.equals(registerForm.getWorkingFlgStr())) {
+
+				// 仕掛作業ありの場合、終了する
+				try {
+					workFinish(inputWork);
+				} catch (BusinessException e) {
+					request.setAttribute(ConstantDef.ERROR_MSG, e.getMessage());
+				}
+			}
+
+			inputWork.setContents(registerForm.getContents());
+			inputWork.setNote(registerForm.getNote());
 			WorkLogic logic = new WorkLogic();
 
 			try {
@@ -180,6 +169,7 @@ public class WorkRegister extends HttpServlet {
 				request.setAttribute(ConstantDef.ERROR_MSG, e.getMessage());
 			}
 		}
+<<<<<<< ba95d4742718b3dd24a319482f4cf911f9117ea0
 		// 登録画面の再表示
 		RequestDispatcher dispatcher = request
 				.getRequestDispatcher("/WEB-INF/jsp/work/workRegistForm.jsp");
@@ -188,17 +178,107 @@ public class WorkRegister extends HttpServlet {
 		} catch (ServletException | IOException e) {
 			throw new SystemException(e);
 		}
+=======
+		response.sendRedirect("/WorkManager/WorkRegister");
+>>>>>>> 作業登録画面に入力チェックを実装
 	}
 
-	private WorkRegisterForm setForm(HttpServletRequest request) {
+	/**
+	 * 作業終了時のリクエスト情報をフォームに設定
+	 * 
+	 * @param request
+	 * @return
+	 */
+	private WorkFinishForm setfinishForm(HttpServletRequest request) {
+
+		WorkFinishForm form = new WorkFinishForm();
+		form.setId(request.getParameter("id"));
+
+		return form;
+	}
+
+	/**
+	 * 作業開始ボタン押下時の入力チェック
+	 * 
+	 * @param form
+	 * @param inputWork
+	 * @return
+	 */
+	private ValidationResult inputCheckWhenStart(WorkRegisterForm form,
+			Work inputWork) {
+
+		ValidationResult result = new ValidationResult();
+		result.setCheckResult(true);
+
+		// idチェック
+		String id = form.getId();
+		if (!InputValidation.idCheck(id)) {
+			throw new SystemException("不正な入力");
+		} else {
+			if (!id.isEmpty()) {
+				inputWork.setId(Integer.valueOf(id));
+			}
+		}
+
+		// 作業中フラグチェック
+		if (!InputValidation.flgCheck(form.getWorkingFlgStr())) {
+			throw new SystemException("不正な入力");
+		}
+
+		// 開始時間チェック
+		String startTime = form.getStartTime();
+		if (startTime == null) {
+			throw new SystemException("不正な入力");
+		}
+		if (!startTime.isEmpty()) {
+			// 入力check
+			result = InputValidation.isTime(startTime);
+			if (result.isCheckResult()) {
+				inputWork.setStartTime(DateUtils.getFomatTime(startTime));
+			}
+		} else {
+			// 未入力の場合、再入力
+			result.setCheckResult(false);
+			result.setErrorMsg("入力してください。");
+		}
+
+		// 作業内容
+		String contents = form.getContents();
+		if (contents == null) {
+			throw new SystemException("不正な入力");
+		} else {
+			result = InputValidation.inputSize(contents, 0, 40);
+			if (result.isCheckResult()) {
+				inputWork.setContents(contents);
+			}
+		}
+
+		// 備考チェック
+		String note = form.getNote();
+		if (note == null) {
+			throw new SystemException("不正な入力");
+		} else {
+			result = InputValidation.inputSize(contents, 0, 40);
+			if (result.isCheckResult()) {
+				inputWork.setNote(note);
+			}
+		}
+
+		return result;
+	}
+
+	/**
+	 * 作業開始時のリクエスト情報をフォームに設定
+	 * 
+	 * @param request
+	 * @return
+	 */
+	private WorkRegisterForm setRegisterForm(HttpServletRequest request) {
 
 		WorkRegisterForm form = new WorkRegisterForm();
 		form.setId(request.getParameter("id"));
-
-		// boolean workFlg = ConvertToModelUtils.convertBoolean(request
-		// .getParameter("workingFlg"));
-		// form.setWorkingFlg(workFlg);
-
+		form.setStartTime(request.getParameter("startTime"));
+		form.setWorkingFlgStr(request.getParameter("workingFlg"));
 		form.setContents(request.getParameter("contents"));
 		form.setNote(request.getParameter("note"));
 
