98863a9da40a616966efe7c9da7772d33915d850
fukumotom <a.word.of.kindness073@gmail.com>
2016-06-08 18:01:15 +0900

    servletの処理を一部ロジックへ移行

--- WorkManager/src/main/java/jp/kigami/ojt/logic/WorkLogic.java
+++ WorkManager/src/main/java/jp/kigami/ojt/logic/WorkLogic.java
@@ -91,7 +91,77 @@ public class WorkLogic {
 	public void deleteUnSaveWork(Work inputWork) {
 		WorkDao dao = new WorkDao();
 		dao.deleteUnSaveWork(inputWork);
+	}
+	
+	/**
+	 * 画面表示用フォームの取得
+	 * 
+	 * @param userName
+	 * @return
+	 */
+	public WorkRegisterViewForm getWorkRegisterViewForm(String userName) {
 
+		WorkRegisterViewForm form = new WorkRegisterViewForm();
+
+		Work inputWork = new Work();
+		inputWork.setUserName(userName);
+
+		// 現在時間を設定
+		inputWork.setWorkDate(LocalDate.now());
+
+		// 仕掛作業取得
+		List<Work> workList = findWorking(inputWork);
+
+		// formに仕掛作業と作業状態フラグを設定
+		if (workList.size() == 0) {
+			logger.info("仕掛処理なし");
+			form.setWork(null);
+			form.setWorkingFlg(false);
+		} else {
+			logger.info("仕掛処理あり");
+			form.setWork(workList.get(0));
+			form.setWorkingFlg(true);
+		}
+
+		// 作業開始時間(初期表示用)を設定
+		form.setNowTime(DateUtils.getNowTimeStr());
+
+		return form;
+	}
+
+	/**
+	 * 仕掛作業取得
+	 * 
+	 * @param inputWork
+	 * @return
+	 */
+	public List<Work> findWorking(Work inputWork) {
+		WorkDao dao = new WorkDao();
+		List<Work> workList = dao.findWorking(inputWork);
+		return workList;
+	}
+
+	/**
+	 * 作業完了処理
+	 * 
+	 * @param inputWork
+	 * @throws BusinessException
+	 */
+	public void finishWork(Work inputWork) throws BusinessException {
+
+		// 終了時間を設定
+		inputWork.setEndTime(DateUtils.getNowTime());
+
+		// 開始時間を設定
+		LocalTime startTime = getStartTime(inputWork);
+		inputWork.setStartTime(DateUtils.getParseTime(startTime));
+
+		// 作業時間を計算
+		WorkHelper helper = new WorkHelper();
+		helper.calcWorkTime(inputWork);
+
+		WorkDao dao = new WorkDao();
+		dao.finishWork(inputWork);
 	}
 	
 	
