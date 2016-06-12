2fb03bcb53c2c9fc265c477a87f52cb6c75e1030
fukumotom <a.word.of.kindness073@gmail.com>
2016-06-13 07:55:30 +0900

    modify propertyUtil and messageDef and message.properties

--- WorkManager/src/main/java/jp/kigami/ojt/common/util/MsgCodeDef.java
+++ WorkManager/src/main/java/jp/kigami/ojt/common/util/MsgCodeDef.java
@@ -1,12 +1,46 @@
 package jp.kigami.ojt.common.util;
 
+/**
+ * メッセージ定義クラス
+ * 
+ * @author kigami
+ *
+ */
 public class MsgCodeDef {
 
+	/**
+	 * 入力チェック（パスワード不一致エラー）
+	 */
 	public static final String CONFIRM_ERROR = "e.validation.001";
 
+	/**
+	 * 入力チェック（サイズエラー）
+	 */
 	public static final String SIZE_ERROR = "e.validation.002";
 
+	/**
+	 * 入力チェック（未入力エラー）
+	 */
+	public static final String EMPTY_INPUT = "e.validation.003";
+
+	/**
+	 * 入力チェック（フォーマットエラー）
+	 */
+	public static final String INPUT_FORMAT_ERROR = "e.validation.004";
+
+	/**
+	 * 仕掛作業取得時エラー
+	 */
+	public static final String MULTI_DATE_EXIT = "w.db.001";
+
+	/**
+	 * 作業完了時警告
+	 */
+	public static final String ALREADY_FINISHED = "w.db.002";
+
+	/**
+	 * プライベートコンストラクタ
+	 */
 	private MsgCodeDef() {
 	}
-
 }
