4e73040ac584d5a3281a4a98229a5293dfb49b0b
fukumotom <a.word.of.kindness073@gmail.com>
2016-06-14 17:58:52 +0900

    エラーメッセージをまとめてリストで保持、取得できるように修正

--- WorkManager/src/main/java/jp/kigami/ojt/common/util/ValidationResult.java
+++ WorkManager/src/main/java/jp/kigami/ojt/common/util/ValidationResult.java
@@ -1,24 +1,66 @@
 package jp.kigami.ojt.common.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ValidationResult {
 
-	private String errorMsg;
+	/**
+	 * 入力エラーメッセージのリスト
+	 */
+	private List<String> errorMsgList;
 
+	/**
+	 * 入力チェック結果
+	 */
 	private boolean checkResult;
 
-	public String getErrorMsg() {
-		return errorMsg;
+	/**
+	 * メッセージリストの初期化
+	 */
+	public ValidationResult() {
+		this.errorMsgList = new ArrayList<>();
 	}
 
-	public void setErrorMsg(String errorMsg) {
-		this.errorMsg = errorMsg;
+	/**
+	 * すべてのエラーメッセージを結合して取得
+	 * 
+	 * @return the errorMsg
+	 */
+	public String getErrorMsgｓ() {
+
+		StringBuilder sb = new StringBuilder();
+		for (String errorMsg : errorMsgList) {
+			sb.append(errorMsg);
+			sb.append("<BR />");
+		}
+
+		return sb.toString();
 	}
 
+	/**
+	 * リストにメッセージを追加
+	 * 
+	 * @param errorMsg
+	 *            the errorMsg to add
+	 */
+	public void addErrorMsg(String errorMsg) {
+		this.errorMsgList.add(errorMsg);
+	}
+
+	/**
+	 * @return the checkResult
+	 */
 	public boolean isCheckResult() {
 		return checkResult;
 	}
 
+	/**
+	 * @param checkResult
+	 *            the checkResult to set
+	 */
 	public void setCheckResult(boolean checkResult) {
 		this.checkResult = checkResult;
 	}
+
 }
