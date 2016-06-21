385c046b22e597c4be2fbe7f8c120fba960afca8
fukumotom <a.word.of.kindness073@gmail.com>
2016-06-21 14:28:22 +0900

    共通ロジックに記載していた未保存作業削除処理を作業管理ロジックに移動
    ロジッククラスはサーブレットと1対1でなくてもよいとのコメントにより修正

--- WorkManager/src/main/java/jp/kigami/ojt/logic/WorkLogic.java
+++ WorkManager/src/main/java/jp/kigami/ojt/logic/WorkLogic.java
@@ -407,4 +407,20 @@ public class WorkLogic {
 
 		dao.insert(inputWork);
 	}
+	
+	/**
+	 * 未保存作業削除処理
+	 * 
+	 * @param userName
+	 */
+	public void deleteUnSaveWork(String userName) {
+
+		logger.debug("未保存作業削除処理開始");
+
+		Work work = new Work();
+		work.setUserName(userName);
+
+		WorkLogic logic = new WorkLogic();
+		logic.deleteUnSaveWork(work);
+	}
 }
