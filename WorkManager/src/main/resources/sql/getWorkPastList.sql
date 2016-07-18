a7ee0a7baf3d7a590cfa2585ab890cd799474b50
fukumotom <a.word.of.kindness073@gmail.com>
2016-07-19 07:58:42 +0900

    fix criteria #37

--- WorkManager/src/main/resources/sql/getWorkPastList.sql
+++ WorkManager/src/main/resources/sql/getWorkPastList.sql
@@ -1,16 +1,18 @@
 SELECT
-	id
-	,user_name
-	,start_time
-	,end_time
-	,working_time
-	,contents
-	,note
-FROM
-	WorkManage w
-WHERE
-	w.user_name = ${userName}
-	And w.work_date = ${workDate}
-	AND w.delete_flg = 0
-ORDER BY
-	w.start_time, w.end_time
+        id
+        ,user_name
+        ,start_time
+        ,end_time
+        ,working_time
+        ,contents
+        ,note
+    FROM
+        WorkManage w
+    WHERE
+        w.user_name = $ { userName }
+        AND w.work_date = $ { workDate }
+        AND w.delete_flg = 0
+        AND w.status_flg = 1
+    ORDER BY
+        w.start_time
+        ,w.end_time;
