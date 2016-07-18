e5880c3a353d4885124f638f9de0fec20424199b
fukumotom <a.word.of.kindness073@gmail.com>
2016-07-19 08:01:28 +0900

    fix sql #37

--- WorkManager/src/main/resources/sql/copyTodayWork.sql
+++ WorkManager/src/main/resources/sql/copyTodayWork.sql
@@ -1,20 +1,22 @@
-INSERT INTO workmanage
-'workmanage_id_seq') As id
-           ,user_name
-           ,start_time
-           ,end_time
-           ,working_time
-           ,contents
-           ,note
-           ,work_date
-           ,uptime
-           ,insert_flg
-           ,update_flg
-           ,delete_flg
-           ,1 As status_flg
-       FROM
-           workmanage
-       WHERE
-           user_name = ${userName}
-           AND work_date = Date 'now'
-   )
+INSERT INTO
+	workmanage(
+		SELECT
+			nextval('workmanage_id_seq') AS id
+			,user_name
+			,start_time
+			,end_time
+			,working_time
+			,contents
+			,note
+			,work_date
+			,uptime
+			,insert_flg
+			,update_flg
+			,delete_flg
+			,1 AS status_flg
+		FROM
+			workmanage
+		WHERE
+			user_name = $ { userName }
+			AND work_date = DATE 'now'
+	);
