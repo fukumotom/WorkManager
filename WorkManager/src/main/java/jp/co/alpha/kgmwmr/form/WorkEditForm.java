abb3e06d87453f1bfe51edc19c36695b1356a6a7
fukumotom <a.word.of.kindness073@gmail.com>
2016-08-18 22:03:47 +0900

    create combo box #34

--- WorkManager/src/main/java/jp/co/alpha/kgmwmr/form/WorkEditForm.java
+++ WorkManager/src/main/java/jp/co/alpha/kgmwmr/form/WorkEditForm.java
@@ -51,11 +51,6 @@ public class WorkEditForm {
 	private String errMsgs;
 
 	/**
-	 * 選択用備考リスト
-	 */
-	private ArrayList<String> noteList;
-
-	/**
 	 * @return the id
 	 */
 	public String getId() {
@@ -70,10 +65,17 @@ public class WorkEditForm {
 		this.id = id;
 	}
 
+	/**
+	 * @return the userName
+	 */
 	public String getUserName() {
 		return userName;
 	}
 
+	/**
+	 * @param userName
+	 *            the userName to set
+	 */
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
@@ -93,10 +95,17 @@ public class WorkEditForm {
 		this.startTime = startTime;
 	}
 
+	/**
+	 * @return the endTime
+	 */
 	public String getEndTime() {
 		return endTime;
 	}
 
+	/**
+	 * @param endTime
+	 *            the endTime to set
+	 */
 	public void setEndTime(String endTime) {
 		this.endTime = endTime;
 	}
@@ -146,20 +155,39 @@ public class WorkEditForm {
 		this.note = note;
 	}
 
+	/**
+	 * @return the errMsgs
+	 */
 	public String getErrMsgs() {
 		return errMsgs;
 	}
 
+	/**
+	 * @param errMsgs
+	 *            the errMsgs to set
+	 */
 	public void setErrMsgs(String errMsgs) {
 		this.errMsgs = errMsgs;
 	}
 
+	/**
+	 * @return the noteList
+	 */
 	public ArrayList<String> getNoteList() {
 		return noteList;
 	}
 
+	/**
+	 * @param noteList
+	 *            the noteList to set
+	 */
 	public void setNoteList(ArrayList<String> noteList) {
 		this.noteList = noteList;
 	}
+
+	/**
+	 * 選択用備考リスト
+	 */
+	private ArrayList<String> noteList;
 
 }
